9b10187fc05786f215caaf90ad9264c95147d703
jrodri <jrodri@elpuig.xeill.net>
2021-11-26 19:04:15 +0100

    Initial commit

---
+++ src/com/company/ConStrings.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ConStrings {
+    public static void main(String[] args) throws FileNotFoundException {
+        File santako = new File("src/santako.txt");
+        Scanner file = new Scanner(santako);
+
+        int papanoel = 0;
+        int renos = 0;
+        int elfos = 0;
+
+        while (file.hasNext()){
+
+            papanoel = 0;
+            renos = 0;
+            elfos = 0;
+
+            String line = file.nextLine();
+
+
+            elfos = elfos - papanoel;
+            System.out.print("Pare Noel (" + papanoel + ") ");
+            System.out.print("Pare Noel (" + renos + ") ");
+            System.out.println("Pare Noel (" + elfos + ") ");
+        }
+
+
+
+        file.close();
+    }
+}
